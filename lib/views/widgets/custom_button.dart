833b4bd2899b8ff2f5747dc4d5520e139248284a
Omar <abdkaheel9@gmail.com>
2025-12-05 18:25:25 -0800

    change add note bottom sheet  loading

--- lib/views/widgets/custom_button.dart
+++ lib/views/widgets/custom_button.dart
@@ -1,13 +1,14 @@
-
 import 'package:flutter/material.dart';
 import 'package:note_app/constsnts.dart';
 
-class customButton extends StatelessWidget {
-  const customButton({super.key, this.onTap});
-final void Function()? onTap;
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, this.onTap, this.isLoading = false});
+  final void Function()? onTap;
+  final bool isLoading;
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(onTap: onTap,
+    return GestureDetector(
+      onTap: onTap,
       child: Container(
         decoration: BoxDecoration(
           color: kPrimaryColor,
@@ -16,14 +17,16 @@ final void Function()? onTap;
         width: MediaQuery.of(context).size.width,
         height: 55,
         child: Center(
-          child: Text(
-            'Add',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+          child: isLoading
+              ? CircularProgressIndicator(color: Colors.black)
+              : Text(
+                  'Add',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
         ),
       ),
     );
